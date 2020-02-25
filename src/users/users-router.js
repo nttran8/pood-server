ffb63935441262bb335c76c160190a3ac91779a6
Nghi Tran <nghi.t.tran8@gmail.com>
2020-02-24 16:24:52 -0800

    update error handling for user registry

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,10 +1,10 @@
 // Library
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 // Service
-const UsersService = require('./users-service');
-const { requireAuth } = require('../middleware/jwt-auth');
+const UsersService = require("./users-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
 // Router
 const usersRouter = express.Router();
@@ -12,110 +12,105 @@ const usersRouter = express.Router();
 // Middleware
 const jsonBodyParser = express.json();
 
-usersRouter
-  .route('/')
-  .post(jsonBodyParser, (req, res, next) => {
-    const { email, username, password } = req.body;
+usersRouter.route("/").post(jsonBodyParser, (req, res, next) => {
+  const { email, username, password } = req.body;
 
-    // Validate data
-    for (const field of ['email', 'username', 'password'])
-      if (!req.body[field]) {
-        return res.status(400).json({
-          error: `Request body must include ${field}`
-        });
-      }
-    // Validate password
-    const passwordError = UsersService.validatePassword(password);
-    if (passwordError) {
-      return res.status(400).json({ passwordError });
+  // Validate data
+  for (const field of ["email", "username", "password"])
+    if (!req.body[field]) {
+      return res.status(400).json({
+        error: `Request body must include ${field}`
+      });
     }
+  // Validate password
+  const passwordError = UsersService.validatePassword(password);
+  if (passwordError) {
+    return res.status(400).json({ error: passwordError });
+  }
 
-    // Validate username existence
-    UsersService.checkUsername(
-      req.app.get('db'),
-      username)
-      .then(usernameExist => {
-        // Return error if username exist
-        if(usernameExist) {
-          return res.status(400).json({ error: `Username already taken`});
-        }
+  // Validate username existence
+  UsersService.checkUsername(req.app.get("db"), username)
+    .then(usernameExist => {
+      // Return error if username exist
+      if (usernameExist) {
+        return res.status(400).json({ error: `Username already taken` });
+      }
 
-        return UsersService.hashPassword(password)
-          .then(hashedPW => {
-            // Put together new user object
-            const newUser = { 
-              email: email.toLowerCase(), 
-              username, 
-              password: hashedPW 
-            };
-            // Add user
-            return UsersService.insertUser(
-              req.app.get('db'),
-              newUser)
-              .then(user => res
-                .status(201)
-                .location(path.posix.join(req.originalUrl, `/${user.id}`))
-                .json(UsersService.serializeUser(user)));
-              });
-      })
-      .catch(next);
-  });
+      return UsersService.hashPassword(password).then(hashedPW => {
+        // Put together new user object
+        const newUser = {
+          email: email.toLowerCase(),
+          username,
+          password: hashedPW
+        };
+        // Add user
+        return UsersService.insertUser(req.app.get("db"), newUser).then(user =>
+          res
+            .status(201)
+            .location(path.posix.join(req.originalUrl, `/${user.id}`))
+            .json(UsersService.serializeUser(user))
+        );
+      });
+    })
+    .catch(next);
+});
 
 usersRouter
-  .route('/:id')
+  .route("/:id")
   .all(requireAuth)
   .delete((req, res, next) => {
     // Ensure user is requesting to delete their own account
-    if(!req.user.id == req.params.id) {
-      return res.status(401).json({ 
-        error: 'Cannot delete user'});
+    if (!req.user.id == req.params.id) {
+      return res.status(401).json({
+        error: "Cannot delete user"
+      });
     }
-    UsersService.deleteUser(
-      req.app.get('db'),
-      req.user.id)
+    UsersService.deleteUser(req.app.get("db"), req.user.id)
       .then(() => res.status(204).end())
       .catch(next);
   })
   .patch(jsonBodyParser, (req, res, next) => {
-    const { fullname, email, password, gender} = req.body;
+    const { fullname, email, password, gender } = req.body;
     const criteria = { fullname, email, password, gender };
     // Check for any input
     const checkForChange = Object.values(criteria).filter(Boolean).length;
     if (checkForChange === 0) {
-        return res.status(400).json({ error: `Request body must contain either 'email', 'fullname', 'password', or 'gender'`})
+      return res.status(400).json({
+        error: `Request body must contain either 'email', 'fullname', 'password', or 'gender'`
+      });
     }
 
     // ---------- Validate data
 
     // Validate gender
-    const genderOption = ['female', 'male'];
-    if(criteria.gender !== null && !genderOption.includes(criteria.gender)) {
-      return res.status(400).json({ error: `'Gender' value could only be either female or male`});
+    const genderOption = ["female", "male"];
+    if (criteria.gender !== null && !genderOption.includes(criteria.gender)) {
+      return res
+        .status(400)
+        .json({ error: `'Gender' value could only be either female or male` });
     }
     // Validate email and password
     criteria.email = email.toLowerCase();
-    for (const field of ['email', 'password'])
-      // Return error if the above field is included but empty in the request body
+    // Return error if the above field is included but empty in the request body
+    for (const field of ["email", "password"])
       if (req.body.hasOwnProperty(field) && !req.body[field]) {
         return res.status(400).json({
           error: `Request body must include a value for '${field}'`
         });
       }
-      
-    if (req.body.hasOwnProperty('password')) {
+
+    if (req.body.hasOwnProperty("password")) {
       const passwordError = UsersService.validatePassword(password);
       if (passwordError) {
-      return res.status(400).json({ passwordError });
+        return res.status(400).json({ passwordError });
       }
-      return UsersService.hashPassword(password)
-        .then(hashedPW => {
-          criteria.password = hashedPW;
-          return updateUser(criteria, req, res, next);
-        });
+      return UsersService.hashPassword(password).then(hashedPW => {
+        criteria.password = hashedPW;
+        return updateUser(criteria, req, res, next);
+      });
     }
     return updateUser(criteria, req, res, next);
   });
-
 
 function updateUser(criteria, req, res, next) {
   // Complete building user object
@@ -124,14 +119,11 @@ function updateUser(criteria, req, res, next) {
     if (criteria[key] !== undefined) {
       userToUpdate[key] = value;
     }
-    console.log(userToUpdate);
-  
+  console.log(userToUpdate);
+
   const date_modified = new Date();
   userToUpdate.date_modified = date_modified;
-  UsersService.updateUser(
-    req.app.get('db'),
-    req.user.id,
-    userToUpdate)
+  UsersService.updateUser(req.app.get("db"), req.user.id, userToUpdate)
     .then(() => res.status(204).end())
     .catch(next);
 }
